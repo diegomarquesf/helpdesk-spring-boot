86af46a7adf642fb3ab782ffcd408492b9cc2e35
Diego <fariia_di@hotmail.com>
2022-07-02 11:17:31 -0300

    criacao do projeto e dependencias

---
+++ src/main/java/br/com/diego/helpdesk/HelpdeskSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.diego.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskSpringApplication.class, args);
+	}
+
+}
